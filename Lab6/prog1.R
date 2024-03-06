a89aa739eceaf9fcbbd30ab634322a91d92b25c3
rishabhkhetan <rishabh.khetan@outlook.com>
2024-03-06 10:08:04 +0530

    add gitignore, prog1

---
+++ Lab6/prog1.R
@@ -0,0 +1,4 @@
+myvec <- c("Rishabh","Raunny","Ayush")
+print(is.factor(myvec))
+myfac = factor(myvec)
+print(myfac)
--- Lab6/samplefile3.R
+++ Lab6/samplefile3.R
@@ -3,3 +3,5 @@ xc=c(2,3,4,5)
 yc=c(2,3,4,5)
 plot(xc,yc,col = "red")
 plot(xc,yc,col = "red",type='l')
+#the below line also works
+# pie(20,30,40)
